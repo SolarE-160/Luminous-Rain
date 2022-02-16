9c3989be982cd5a88b66bcd5e2e4de94a4c8aeea
Edward Sun <58571756+SolarE-160@users.noreply.github.com>
2022-02-16 15:10:32 -0500

    bullets move and glow now woo

--- scripts/script.js
+++ scripts/script.js
@@ -1,30 +1,60 @@
 const game = {};
 
 class Stage extends Phaser.Scene {
-
   constructor(key) {
     super(key);
     this.levelName = key;
   }
 
-  preload() {
-
-  }
+  preload() {}
 
   create() {
-    /*
-    game.circleArray = [];
-    for (let i = 0; i < 100; i++) {
-      let randomX = Math.random() * 400;
-      let randomY = Math.random() * 400;
-      game.circleArray.push([this.add.circle(randomX, randomY, 12, 0x0080FF), this.add.circle(randomX, randomY, 8, 0xFFFFFF)]);
-    }*/
     game.bullets = this.add.group();
-    for (let i = 0; i < 10; i++) {
-      //let singlebulletglow = this.add.glowBullet(300, 150 + i * 10, 1, 0, 5, 0);
-      let singlebullet = this.add.bullet(300, 150 + i * 10, 1, 0, 5, 0);
-      //game.bullets.add(singlebulletglow);
+    for (let i = 0; i < 1000; i++) {
+      let singlebullet = this.add.bullet(
+        Math.random() * 800,
+        Math.random() * 600,
+        Math.random() * 2 - 1,
+        Math.random() * 2 - 1,
+        5,
+        0,
+        0
+      );
+      singlebullet.depth = 3;
       game.bullets.add(singlebullet);
+      if (!settings.glow) {
+        let singlebulletoutline = this.add.bullet(
+          singlebullet.x,
+          singlebullet.y,
+          singlebullet.vx,
+          singlebullet.vy,
+          singlebullet.r * 1.6,
+          singlebullet.delay,
+          singlebullet.grav,
+          singlebullet.color
+        );
+        singlebulletoutline.fillColor = singlebullet.color;
+        singlebulletoutline.depth = 2;
+        game.bullets.add(singlebulletoutline);
+      }
+    }
+
+    if (settings.glow) {
+      game.bulletGlowEffects = this.add.group();
+      for (let singlebullet of game.bullets.getChildren()) {
+        let singlebulletglow = this.add.bulletGlow(
+          singlebullet.x,
+          singlebullet.y,
+          singlebullet.vx,
+          singlebullet.vy,
+          singlebullet.width / 2,
+          singlebullet.delay,
+          singlebullet.grav,
+          singlebullet.color
+        );
+        singlebulletglow.depth = 1;
+        game.bulletGlowEffects.add(singlebulletglow);
+      }
     }
   }
 
@@ -39,25 +69,36 @@ class Stage extends Phaser.Scene {
       particle[1].y += randomY;
     }
     */
+    // 1st pass, actual bullets
     for (let singlebullet of game.bullets.getChildren()) {
       singlebullet.draw();
+    }
+    if (settings.glow) {
+      // 2nd pass, bullet glow
+      for (let singlebulletglow of game.bulletGlowEffects.getChildren()) {
+        singlebulletglow.draw();
+      }
     }
   }
 }
 
 const config = {
-	type: Phaser.AUTO,
-	width: 800,
-	height: 600,
+  type: Phaser.AUTO,
+  width: 800,
+  height: 600,
   resolution: window.devicePixelRatio,
-	backgroundColor: "#1e1e46",
+  backgroundColor: "#1e1e46",
   plugins: {
     global: [
-      { key: 'BulletPlugin', plugin: BulletPlugin, start: true },
-      //{ key: 'GlowBulletPlugin', plugin: GlowBulletPlugin, start: true }
-    ]
+      { key: "BulletPlugin", plugin: BulletPlugin, start: true },
+      { key: "GlowPlugin", plugin: GlowPlugin, start: true },
+    ],
   },
-	scene: [Stage],
-}
+  scene: [Stage],
+};
 
-const g = new Phaser.Game(config)
+const g = new Phaser.Game(config);
+
+const fpsLogger = setInterval(() => {
+  console.log(g.loop.actualFps);
+}, 1000);
---
+++ settings.js
@@ -0,0 +1,8 @@
+const settings = {
+  // surprisingly, the performance is better *with* glow
+  glow: true,
+
+  // true: stroke
+  // false: two-shape
+  //sts: false,
+}
